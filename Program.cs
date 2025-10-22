a1dd5adc6d8d1aac397488e9eb773e2f5e427f90
Öykü Akyol <96844478+oykuakyol@users.noreply.github.com>
2025-10-23 01:51:59 +0300

    Add files via upload
    Added Mini Calculator v1 project

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+
+class Program
+{ 
+    static void Main()
+    {
+        Console.WriteLine("=== Mini Calculator ===");
+
+        Console.Write("Enter the first number: ");
+        bool isNumber1 = double.TryParse(Console.ReadLine(), out double num1);
+
+        
+        Console.Write("Enter the second number: ");
+        bool isNumber2 = double.TryParse(Console.ReadLine(), out double num2);
+
+        
+        if (!isNumber1 || !isNumber2)
+        {
+            Console.WriteLine("⚠️ Please enter valid numbers!");
+            return;
+        }
+
+       
+        Console.Write("Choose the operation type (+, -, *, /): ");
+        string op = Console.ReadLine();
+
+        double result = 0;
+        bool validOperation = true;
+
+        // 4. Hesaplama
+        switch (op)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0)
+                {
+                    Console.WriteLine("⚠️ 0 division error!");
+                    validOperation = false;
+                }
+                else
+                    result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("⚠️ You chose invalid operation!");
+                validOperation = false;
+                break;
+        }
+
+        // 5. Sonuç
+        if (validOperation)
+            Console.WriteLine($"Result: {result}");
+    }
+}
